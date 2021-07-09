5cf58de863d19adc9f6459e8467f87e8ab5e4ca6
penguin-number-123 <62600889+penguin-number-123@users.noreply.github.com>
2021-07-09 11:18:46 +0800

    eeesaddwda

---
+++ hypercalc.py
@@ -0,0 +1,18 @@
+import math as Math
+import numpy as np
+log300 = Math.log(300)*Math.log(Math.e,10)
+val = 1
+pt = 0
+isarr = np.array([1,2,3,4,5,6,7,8,9])
+NaN = float(nan)
+sqrt2pi = Math.sqrt(2 * Math.pi)
+sqrtpi = Math.sqrt(Math.pi)
+pi2 = Math.pi * Math.pi
+twopi = 2 * Math.pi
+delayedError = ""
+in_str = ""     
+imrs = np.array()     
+inps = np.array("0/0") 
+outs = np.array([0,NaN])
+def isarray(n): return true if type(n) == type(isarr) else false
+
